594777baedad2f26547af7f776cb80bfb4e6e70c
Radu Dumitrescu <r.dumitrescu@reply.com>
2024-02-09 11:40:22 +0200

    wip ci cd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://genezio.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -31,7 +31,7 @@ const config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
-    locales: ["en"],
+    locales: ["en"]
   },
 
   plugins: ["plugin-image-zoom"],
@@ -39,85 +39,84 @@ const config = {
     [
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: "./sidebars.js",
           routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.,
-          include: ["**/*.md", "**/.*.md"], // Include dot files
+          include: ["**/*.md", "**/.*.md"] // Include dot files
         },
         blog: false,
         theme: {
-          customCss: "./src/css/custom.css",
-        },
-      }),
-    ],
+          customCss: "./src/css/custom.css"
+        }
+      }
+    ]
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/genezio.svg",
-      navbar: {
-        title: "Genezio Documentation",
-        logo: {
-          alt: "Genezio Logo",
-          src: "img/favicon.ico",
-          srcDark: "img/genezio.svg",
-        },
-      },
-      // footer: {
-      //   style: "dark",
-      //   links: [
-      //     {
-      //       title: "Docs",
-      //       items: [
-      //         {
-      //           label: "Tutorial",
-      //           to: "/docs/intro",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: "Community",
-      //       items: [
-      //         {
-      //           label: "Stack Overflow",
-      //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
-      //         },
-      //         {
-      //           label: "Discord",
-      //           href: "https://discordapp.com/invite/docusaurus",
-      //         },
-      //         {
-      //           label: "Twitter",
-      //           href: "https://twitter.com/docusaurus",
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: "More",
-      //       items: [
-      //         {
-      //           label: "Blog",
-      //           to: "/blog",
-      //         },
-      //         {
-      //           label: "GitHub",
-      //           href: "https://github.com/facebook/docusaurus",
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      // },
-      prism: {
-        theme: prismThemes.oneLight,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/genezio.svg",
+    navbar: {
+      title: "Genezio Documentation",
+      logo: {
+        alt: "Genezio Logo",
+        src: "img/favicon.ico",
+        srcDark: "img/genezio.svg"
+      }
+    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Tutorial",
+    //           to: "/docs/intro",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "Community",
+    //       items: [
+    //         {
+    //           label: "Stack Overflow",
+    //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+    //         },
+    //         {
+    //           label: "Discord",
+    //           href: "https://discordapp.com/invite/docusaurus",
+    //         },
+    //         {
+    //           label: "Twitter",
+    //           href: "https://twitter.com/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "More",
+    //       items: [
+    //         {
+    //           label: "Blog",
+    //           to: "/blog",
+    //         },
+    //         {
+    //           label: "GitHub",
+    //           href: "https://github.com/facebook/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
+    prism: {
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.dracula
+    }
+  }
 };
 
 export default config;
