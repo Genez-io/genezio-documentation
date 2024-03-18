8b54032edc9209505acd0bb84f86fad886e9c193
tudorAng <tudoranis@yahoo.com>
2024-03-18 15:49:27 +0200

    remove trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,6 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/docs/",
-  trailingSlash: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "genezio", // Usually your GitHub org/user name.
